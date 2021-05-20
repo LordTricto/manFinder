2cc27409bba751c077e04470715acb0d60cc3470
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-05-20 18:47:37 +0800

    finished the missing person form page

---
+++ js/ham.js
@@ -0,0 +1,9 @@
+let ham = document.querySelector('.ham');
+let navlinks = document.querySelector('.navlinks')
+
+ham.addEventListener('click',e=>{
+
+    
+navlinks.classList.toggle('show_nav')
+
+})
