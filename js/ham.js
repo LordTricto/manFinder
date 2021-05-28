0a99c0eb27d9c1f0285a2d678adc461038210b53
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-05-28 12:52:16 +0800

    started creating the missingitenDetailPage as of now i have created a second nav bar were they can see the missing person details next up it creaing the rest of it content

--- js/ham.js
+++ js/ham.js
@@ -7,3 +7,25 @@ ham.addEventListener('click',e=>{
 navlinks.classList.toggle('show_nav')
 
 })
+
+
+// code for showing the user info can be found in
+/*
+    missingItemDetailPage.html
+*/
+let toggle_extra_detail_pane_Btn = document.querySelector('#toggle_extra_detail_pane_btn');
+let extra_details_pane = document.querySelector('.extra_details_pane');
+let close_extra_details_pane = document.querySelector('#close_extra_details_pane')
+// show extra_details_pane
+toggle_extra_detail_pane_Btn.addEventListener('click',e=>{
+
+    extra_details_pane.classList.add('show_pane')
+
+})
+
+// remove extra_details_pane
+close_extra_details_pane.addEventListener('click',()=>{
+
+    extra_details_pane.classList.remove('show_pane')
+
+})
